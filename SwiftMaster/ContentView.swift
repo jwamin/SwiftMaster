92ab85ca96c448e7edbdf3a316bd6a68148341e2
Joss Manger <jossferatu@gmail.com>
2020-09-25 11:55:38 -0400

    adds date, "professional" second hand

--- SwiftMaster/ContentView.swift
+++ SwiftMaster/ContentView.swift
@@ -12,6 +12,7 @@ struct Time {
   let hours: Int
   let minutes: Int
   let seconds: Int
+  let ms: Int
   let dayOfMonth: Int
   
   var hourAngle: Angle {
@@ -25,6 +26,18 @@ struct Time {
   var sixtyAngle:(Int) -> Angle = { unit in
     let dblUnit = Double(unit)
     return Angle(degrees: 360 * Double(dblUnit / 60))
+    
+  }
+  
+  func secondHandAngle() -> Angle {
+    let secondDblUnit = Double(seconds)
+    let msDoubleUnit = Double(ms)
+    
+    let msDecimal = Double(msDoubleUnit / 1000)
+    
+    let secondRenderingValue = secondDblUnit + msDecimal
+    
+    return Angle(degrees: 360 * Double(secondRenderingValue / 60))
     
   }
   
@@ -46,7 +59,15 @@ struct SimpleClockFace: View {
     let usedHeight = height ?? reader.size.height/2
     //hands
     
-    return Rectangle().fill().foregroundColor(color).frame(width: width, height: usedHeight, alignment: .bottom).rotationEffect(angle, anchor: .bottom).position(x: reader.size.height/2, y: reader.size.width/2).offset(y: -(usedHeight/2))
+    return Rectangle().fill().foregroundColor(color).frame(width: width, height: usedHeight, alignment: .bottom).rotationEffect(angle, anchor: .bottom).position(x: reader.size.width/2, y: reader.size.height/2).offset(y: -(usedHeight/2))
+  }
+  
+  fileprivate func renderDate(_ reader: GeometryProxy, color: Color) -> some View {
+    
+    let usedHeight = reader.size.height/2
+    //hands
+    
+    return Text("\(time.dayOfMonth)").font(.title).bold().foregroundColor(color).position(x: reader.size.width/2, y: reader.size.height).offset(y: -(usedHeight/4))
   }
   
   var body: some View {
@@ -58,13 +79,16 @@ struct SimpleClockFace: View {
         
         createHand(reader, width: 10, angle:time.sixtyAngle(time.minutes),color:.black)
         
-        createHand(reader, width: 2, angle:time.sixtyAngle(time.seconds),color:.red)
+        createHand(reader, width: 2, angle:time.secondHandAngle(),color:.red)
+        
+        renderDate(reader, color: .red)
         
         //teeth
       }
     }
     .aspectRatio(1, contentMode: .fit)
     .padding()
+    .drawingGroup()
   }
 }
 
@@ -79,14 +103,14 @@ struct ContentView_Previews: PreviewProvider {
 
 class TimeProvider: ObservableObject {
   
-  @Published var publishedTime: Time = Time(hours: 10, minutes: 9, seconds: 25, dayOfMonth: 25)
+  @Published var publishedTime: Time = Time(hours: 10, minutes: 9, seconds: 25, ms: 999, dayOfMonth: 25)
   
   private var reciever: PassthroughSubject = PassthroughSubject<Date,Never>()
   private var set = Set<AnyCancellable>()
   
   init(){
     
-    Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+    Timer.publish(every: 0.15, on: .main, in: .common).autoconnect()
       .sink { [weak self] (_) in
         self?.reciever.send(Date())
       }.store(in: &set)
@@ -96,9 +120,9 @@ class TimeProvider: ObservableObject {
       let hour = Calendar.current.component(.hour, from: date)
       let minute = Calendar.current.component(.minute, from: date)
       let seconds = Calendar.current.component(.second, from: date)
-      
+      let milli = Calendar.current.component(.nanosecond, from: date) / 1_000_000
       let day = Calendar.current.component(.day, from: date)
-      let time = Time(hours: hour, minutes: minute, seconds: seconds, dayOfMonth: day)
+      let time = Time(hours: hour, minutes: minute, seconds: seconds, ms: milli, dayOfMonth: day)
       
       print(time)
       
