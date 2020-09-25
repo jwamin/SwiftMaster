0d18a748074591588827934c1a801fc3f71c1b88
Joss Manger <jossferatu@gmail.com>
2020-09-25 08:24:07 -0400

    Initial Commit

---
+++ SwiftMaster/SwiftMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMasterApp.swift
+//  SwiftMaster
+//
+//  Created by Joss Manger on 9/25/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
